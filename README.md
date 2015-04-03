39db9fa04af0b87de418013043d171db8bc6adeb
Jethro Carr <jethro.carr@jethrocarr.com>
2015-04-04 00:44:36 +1300

    Wrote initial version of module

---
+++ README.md
@@ -0,0 +1,30 @@
+# puppet-digitalocean
+
+Provides a small fact for exposing Digital Ocean metdata (https://developers.digitalocean.com/documentation/metadata/).
+
+Include this module on any Digital Ocean instance and it will expose a number of metrics including:
+
+* digital_ocean_dns_nameservers
+* digital_ocean_hostname
+* digital_ocean_id
+* digital_ocean_interfaces_public_0_ipv4_address
+* digital_ocean_interfaces_public_0_ipv4_gateway
+* digital_ocean_interfaces_public_0_ipv4_netmask
+* digital_ocean_interfaces_public_0_ipv6_address
+* digital_ocean_interfaces_public_0_ipv6_cidr
+* digital_ocean_interfaces_public_0_ipv6_gateway
+* digital_ocean_interfaces_public_0_mac
+* digital_ocean_interfaces_public_0_type
+* digital_ocean_public_keys
+* digital_ocean_region
+* digital_ocean_vendor_data
+
+
+# Usage
+
+These are the same as any other fact, you can simply read them with $::name, eg:
+
+    if ($::digital_ocean_id) {
+      notify { "You are running on digital ocean in region: ${::digital_ocean_region}": }
+    }
+
---
+++ lib/facter/digital_ocean.rb
@@ -0,0 +1,42 @@
+# Fact that sucks down all the metadata offered by Digital Ocean other
+# than the user data (see note below).
+#
+# Based on the code written for AWS/EC2 facts that was later merged into
+# mainline facter, credit:
+# http://projects.puppetlabs.com/projects/1/wiki/Amazon_Ec2_Patterns
+
+
+require 'open-uri'
+require 'timeout'
+
+def metadata(id = "")
+  open("http://169.254.169.254/metadata/v1/#{id||=''}").read.
+    split("\n").each do |o|
+    key = "#{id}#{o.gsub(/\=.*$/, '/')}"
+    if key[-1..-1] != '/'
+      value = open("http://169.254.169.254/metadata/v1/#{key}").read.
+        split("\n")
+      value = value.size>1 ? value : value.first
+      symbol = "digital_ocean_#{key.gsub(/\-|\//, '_')}".to_sym
+  
+      # We exclude the user data. This is intentional, the user data often has a
+      # bad habit of triggering issues inside Facter due to bad parsing once saved into
+      # YAML files and is best left out. You can always pull it on demand/as needed
+      # with curl http://169.254.169.254/metadata/v1/user-data if you ever do need it
+      unless key == "user-data"
+        Facter.add(symbol) { setcode { value } }
+      end
+
+    else
+      metadata(key)
+    end
+  end
+end
+
+begin
+  Timeout::timeout(1) { metadata }
+rescue
+  puts "Digital Ocean Metadata Unavailable"
+end
+
+# vim: ai ts=2 sts=2 et sw=2 ft=ruby
